dde57b65144a22644d0d99306be4014c79caacbb
bfulche <bfulche@yahoo.com>
2020-06-17 20:39:04 -0500

    Upload Game File

---
+++ Domino Matching Game Unity/Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public Item[] cards;
+    public GameObject card;
+
+    public GameObject[] emptySlots;
+    public GameObject emptySlot;
+
+
+    private void Start()
+    {
+        card = GameObject.Find("BlueSquare_UI");
+        emptySlot = GameObject.Find("Empty01_UI");
+    }
+
+
+}
+
