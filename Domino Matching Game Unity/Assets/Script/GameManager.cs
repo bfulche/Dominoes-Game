0b1a3e3c1830801220f0782c1584a8e8e2a2d66b
bfulche <bfulche@yahoo.com>
2020-06-18 12:45:56 -0500

    Return inserted card in empty slot

--- Domino Matching Game Unity/Assets/Script/GameManager.cs
+++ Domino Matching Game Unity/Assets/Script/GameManager.cs
@@ -7,17 +7,23 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public Item[] cards;
-    public GameObject card;
+    // public Item[] cards;
+    // public GameObject card;
 
-    public GameObject[] emptySlots;
-    public GameObject emptySlot;
+    //public GameObject[] emptySlots;
+    // public GameObject emptySlot;
+
+    public List<string> testList = new List<string>();
 
 
     private void Start()
     {
-        card = GameObject.Find("BlueSquare_UI");
-        emptySlot = GameObject.Find("Empty01_UI");
+        //card = GameObject.Find("BlueSquare_UI");
+        //emptySlot = GameObject.Find("Empty01_UI");
+
+        testList.Insert(1, "chicken");
+        //testList.Add("Item 1");
+        //testList.Add("Item 2");
     }
 
 
--- Domino Matching Game Unity/Assets/Script/ItemSlot.cs
+++ Domino Matching Game Unity/Assets/Script/ItemSlot.cs
@@ -5,12 +5,22 @@ using UnityEngine.EventSystems;
 
 public class ItemSlot : MonoBehaviour, IDropHandler {
 
+    GameManager gm;
+    public List<GameObject> testList = new List<GameObject>();
+
+    
+
 
     public void OnDrop(PointerEventData eventData) {
         Debug.Log("OnDrop");
         if (eventData.pointerDrag != null) {
             eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
             Debug.Log("Dropped Object was:" + eventData.pointerDrag);
+            testList.Add(eventData.pointerDrag);
+
+
+
+
 
         
         }
