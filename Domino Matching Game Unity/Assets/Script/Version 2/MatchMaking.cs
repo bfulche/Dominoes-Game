4daff9e8994206d07ea21af877c122dee665b676
Chaosborn <55224848+Chaosborn@users.noreply.github.com>
2021-04-26 04:25:57 -0500

    Duplicating room fix
    - using a Photon provided example, duplicating room listings should now be fixed, and room listing updates/changes should be more consistent.
    
    - Added an "Are you sure?" prompt when host hits start game button explaining players can't join the game after it starts... (hopefully less people hit start with 0 other players in the room now).

--- Domino Matching Game Unity/Assets/Script/Version 2/MatchMaking.cs
+++ Domino Matching Game Unity/Assets/Script/Version 2/MatchMaking.cs
@@ -10,6 +10,9 @@ using UnityEngine.UI;
 /// <summary>
 /// Basically combining the custom room and lobby matchmaking scripts. Lots of copy/paste from 
 /// the work already done there.
+/// 
+/// 4/26 Changed how room list is updated to match closer to the provided in the included Photon asteroids demo.
+/// (looked at demo based on this thread about room listings: https://forum.photonengine.com/discussion/12725/getting-room-list-in-pun-2
 /// </summary>
 public class MatchMaking : MonoBehaviourPunCallbacks, IInRoomCallbacks
 {
@@ -18,6 +21,19 @@ public class MatchMaking : MonoBehaviourPunCallbacks, IInRoomCallbacks
     [SerializeField] GameObject playerListingPrefab;
 
     [SerializeField] Transform playersContainer;
+
+    [SerializeField] private List<RoomInfo> roomListings;
+
+    private Dictionary<string, RoomInfo> cachedRoomList;
+    private Dictionary<string, RoomButton> roomListEntries;
+
+
+    private void Awake()
+    {
+        cachedRoomList = new Dictionary<string, RoomInfo>();
+        roomListEntries = new Dictionary<string, RoomButton>();
+    }
+
 
     #region Lobby Related callbacks
     public override void OnConnectedToMaster()
@@ -40,31 +56,14 @@ public class MatchMaking : MonoBehaviourPunCallbacks, IInRoomCallbacks
         PhotonNetwork.JoinLobby();
     }
 
-    private List<RoomInfo> roomListings;
+
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
-        base.OnRoomListUpdate(roomList);
-        Debug.Log("Room List Updated!");
-        int tempIndex;
+        ClearRoomListView();
 
-        foreach (RoomInfo room in roomList)
-        {
-            if (roomListings != null)
-                tempIndex = roomListings.FindIndex(ByName(room.Name));
-            else
-                tempIndex = -1;
-      
-            if (tempIndex != -1)
-            {
-                roomList.RemoveAt(tempIndex);
-                // Destroy listed object for that room
-            }
-            if (room.PlayerCount > 0)
-            {
-                roomListings.Add(room);
-                ListRoom(room);
-            }
-        }
+        UpdateCachedRoomList(roomList);
+
+        UpdateRoomListView();
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
@@ -75,7 +74,64 @@ public class MatchMaking : MonoBehaviourPunCallbacks, IInRoomCallbacks
         ReportRoomCreationFailure();
     }
 
+    public override void OnLeftLobby()
+    {
+        cachedRoomList.Clear();
+
+        ClearRoomListView();
+    }
+
     #endregion
+
+    private void ClearRoomListView()
+    {
+        foreach (RoomButton entry in roomListEntries.Values)
+        {
+            Destroy(entry.gameObject);
+        }
+
+        roomListEntries.Clear();
+    }
+
+    private void UpdateCachedRoomList(List<RoomInfo> roomList)
+    {
+        foreach (RoomInfo info in roomList)
+        {
+            // Remove room from cached room list if it got closed, became invisible or was marked as removed
+            if (!info.IsOpen || !info.IsVisible || info.RemovedFromList)
+            {
+                if (cachedRoomList.ContainsKey(info.Name))
+                {
+                    cachedRoomList.Remove(info.Name);
+                }
+
+                continue;
+            }
+
+            // Update cached room info
+            if (cachedRoomList.ContainsKey(info.Name))
+            {
+                cachedRoomList[info.Name] = info;
+            }
+            // Add new room info to cache
+            else
+            {
+                cachedRoomList.Add(info.Name, info);
+            }
+        }
+    }
+
+    private void UpdateRoomListView()
+    {
+        foreach (RoomInfo info in cachedRoomList.Values)
+        {
+            RoomButton entry = Instantiate(roomListingPrefab, roomsContainer);
+
+            entry.SetRoom(info.Name, info.MaxPlayers, info.PlayerCount);
+
+            roomListEntries.Add(info.Name, entry);
+        }
+    }
 
     static Predicate<RoomInfo> ByName(string name)
     {
@@ -87,6 +143,7 @@ public class MatchMaking : MonoBehaviourPunCallbacks, IInRoomCallbacks
     [SerializeField] private Transform roomsContainer;
     private void ListRoom(RoomInfo room)
     {
+        // 4/26 replaced by asteroids demo implementation
         if (room.IsOpen && room.IsVisible)
         {
             RoomButton tempListing = Instantiate(roomListingPrefab, roomsContainer);
@@ -118,7 +175,7 @@ public class MatchMaking : MonoBehaviourPunCallbacks, IInRoomCallbacks
     public override void OnJoinRoomFailed(short returnCode, string message)
     {
         base.OnJoinRoomFailed(returnCode, message);
-       // Debug.LogError("Attempted to join room \"" + directNameInput + "\" but that room name does not exist.");
+        // Debug.LogError("Attempted to join room \"" + directNameInput + "\" but that room name does not exist.");
 
         roomJoinFailPanel.Text.text = "The requested game \"" + directNameInput + "\" could not be found.";
         roomJoinFailPanel.gameObject.SetActive(true);
